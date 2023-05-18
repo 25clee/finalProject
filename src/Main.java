541871afa9840653c0c7fcee27306e8b7a3059d8
25clee <25clee@castilleja.org>
2023-05-18 08:58:23 -0700

    For homework, I made the fractal trees gradiant, randomized the branches (stochastic tree) and modified the code to create a circle design.

--- src/Main.java
+++ src/Main.java
@@ -13,28 +13,34 @@ public class Main extends PApplet {
 
     public void settings(){size(800, 600);}
 
+//    public void setup(){frameRate(5);}
+
     public void draw(){
         background(255);
         theta = map(mouseX,0,width,0,PI/2);
         translate(width/2, height);
         stroke(0);
-        branch(60);
+        branch(60, 216);
     }
 
-    void branch(float len) {
+    public void branch(float len, int color) {
 //        float theta = random(0,PI/3);
-        line(0, 0, 0, -len);
+        stroke(color/13, color/4, color/2);
+        strokeWeight(20);
+//        line(0, 0, 0, -len);
+        circle(200,100,50);
         translate(0, -len);
         len *= 0.66;
         if (len > 2) {
             pushMatrix();
             rotate(theta);
-            branch(len);
+            branch(len, color + 100);
             popMatrix();
             pushMatrix();
             rotate(-theta);
-            branch(len);
+            branch(len, color + 60);
             popMatrix();
         }
     }
 }
+//continue below exercise 8.9
